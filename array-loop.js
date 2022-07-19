c0a58789b586a9d39e5cac7a9a2871bbe7d5966c
cosmokhairul <porosh.means.touch@gmail.com>
2022-07-19 23:07:14 +0600

    first commit

---
+++ array-loop.js
@@ -0,0 +1,22 @@
+// var numbers = [45, 12, 87, 62, 41, 20, 11, 13];
+// for (var i = 0; i < 8; i++) {
+//     console.log(i);
+// }
+
+// var numbers = [45, 12, 87, 62, 41, 20, 11, 13];
+// for (var i = 0; i < 8; i++) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+// var numbers = [45, 12, 87, 0, 54, 88, 99, 101, 1205];
+// for (var i = 0; i < numbers.length; i++) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+var items = ['book', 'mobile', 'pen', 'charger', 'khata'];
+for (var i = 0; i < items.length; i++) {
+    var element = items[i];
+    console.log(element);
+}
---
+++ for-loop.js
@@ -0,0 +1,31 @@
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+//     console.log('Roast Den Please!!');
+//     roastGiven++;
+//     console.log(roastGiven);
+// }
+
+// console.log('running a for loop');
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log('Roast den please!!');
+//     console.log(roastGiven);
+// }
+
+//Finding Odd Numbers from 2 to 10  
+// for (var i = 2; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+//Finding Even Numbers from 2 to 10  
+for (var i = 1; i <= 10; i += 2) {
+    console.log(i);
+}
+
+// for (var i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+
+// for (var i = 2; i <= 20; i += 2) {
+//     console.log(i);
+// }
